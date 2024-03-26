eb833593865c2187ecfb3da19a33f1e1eb066182
Oscar Ivan Gomez Zamudio <oigomezz@unal.edu.co>
2024-03-26 11:53:40 -0500

    Se agrega la UI

---
+++ client/src/UI/Tile.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Tile({ puzzle, grid, handleChange }) {
+  const tile = col !== 0 ? "tile taken" : "tile";
+
+  return grid.map((row, rowIndex) => {
+    return row.map((col, colIndex) => {
+      return (
+        <input
+          className={puzzle[rowIndex][colIndex] !== 0 ? "initial" : tile}
+          value={col === 0 ? "" : col}
+          key={rowIndex + " " + colIndex}
+          type="text"
+          onChange={(e) => handleChange(rowIndex, colIndex, e)}
+        />
+      );
+    });
+  });
+}
+
+export default Tile;
